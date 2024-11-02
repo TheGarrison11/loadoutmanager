54b575345e2da3eade11c64791757b9ada6f3e37
TheGarrison11 <harrisnoah4145@gmail.com>
2024-11-01 21:39:23 -0400

    Loadout Page
    Added loadout page concept so far

---
+++ scriptbox.js
@@ -0,0 +1,17 @@
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = 'block';
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';
+}
+
+// Close the modal if the user clicks outside of the modal content
+window.onclick = function(event) {
+    const modals = document.getElementsByClassName('modal');
+    for (let modal of modals) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+}
