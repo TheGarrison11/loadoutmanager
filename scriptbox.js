6aad5a4b77a4626c18eee01a699fdd175ab0bc96
TheGarrison11 <harrisnoah4145@gmail.com>
2024-11-01 21:58:45 -0400

    style update(Loadout Page)

--- scriptbox.js
+++ scriptbox.js
@@ -6,7 +6,6 @@ function closeModal(modalId) {
     document.getElementById(modalId).style.display = 'none';
 }
 
-// Close the modal if the user clicks outside of the modal content
 window.onclick = function(event) {
     const modals = document.getElementsByClassName('modal');
     for (let modal of modals) {
